faf76787ecf2c0b43b50d6e95ff7c694a2e9b29c
umarmusa <63979820+umarbinmusa@users.noreply.github.com>
2022-12-27 05:29:25 -0800

    annding nav

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function About() {
+    return(
+        <div>
+            <h1 class="font-3xl text-center">welcome</h1>
+        </div>
+    );
+} export default About;
